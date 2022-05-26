03c7bfbfa092212958b8028c4157f97cf8b95801
Daniel Sznitko <93942736+DSznitko@users.noreply.github.com>
2022-05-26 18:27:08 +0200

    complete app

--- main.js
+++ main.js
@@ -1,92 +1,87 @@
-
-
 const quizData = [
   {
-    question: "Od czego uzależniony jest Sulim?",
-    a: "od walenia konia",
-    b: "od palenia",
-    c: "od alkoholu",
-    d: "a i c",
-    correct: "d"
+    question: "What is the most popular programming language?",
+    a: "PHP",
+    b: "Python",
+    c: "c++",
+    d: "JavaScript",
+    correct: "d",
   },
   {
-    question: "Gdzie pracuje pelera szwagierka?",
-    a: "w Maku",
-    b: "w Boschu",
-    c: "na sorze",
-    d: "w autoneum",
-    correct: "c"
+    question: "What does HTML stand for?",
+    a: "Hypertex Markup Language",
+    b: "Cascading Style Sheet",
+    c: "Jason Object Notation",
+    d: "Human Temporary Military Labor",
+    correct: "a",
   },
   {
-    question: "Ile Kevin zarabia na tym lkw?",
-    a: "2800 ojro brutto",
-    b: "3000 ojro netto",
-    c: "chuj cie obchodzi",
-    d: "spytaj Dejwa on wie",
-    correct: "d"
+    question: "When was JavaScript created?",
+    a: "In 2018",
+    b: "2020",
+    c: "1985",
+    d: "None of those",
+    correct: "d",
   },
   {
-    question: "Kiedy ostatnio Mrówa miał urlop?",
-    a: "2 lata temu",
-    b: "3-5 lat temu",
-    c: "jak nie pracował",
-    d: "to on w ogóle miał urlop?",
-    correct: "d"
+    question: "which one is CSS preprocesor?",
+    a: "Node JS",
+    b: "React JS",
+    c: "Bootstrap",
+    d: "SASS",
+    correct: "d",
   },
   {
-    question: "Najlepszy przyjaciel Radosengo to...",
-    a: "Alex",
-    b: "Wrony stary",
-    c: "pies",
-    d: "Aleks",
-    correct: "a"
+    question: "which one is CSS Framework?",
+    a: "SCSS",
+    b: "Bootstrap",
+    c: "Node JS",
+    d: "NPM",
+    correct: "b",
   },
   {
-    question: "Czy Hary kiedyś się zaszczepi?",
-    a: "nie",
-    b: "jak on juz pewnie po 3 dawkach",
-    c: "jak przypyli to zaszczepi",
-    d: "moze",
-    correct: "a"
+    question: "which one is JavaScript Framework?",
+    a: "React",
+    b: "Vue",
+    c: "Angular",
+    d: "All of them",
+    correct: "d",
   },
   {
-    question: "Jak długo jeszcze Nizioł będzie pracować w autoneum ?",
-    a: "2-3 lata",
-    b: "do osiągniecia wieku emerytalnego",
-    c: "do końca świata i jeden dzień dłużej",
-    d: "aż zostanie jedynym tam pracującym Polakiem",
-    correct: "b"
+    question: "What is Node JS?",
+    a: "Javascript framework",
+    b: "JavaScript animation library",
+    c: "JavaScript runtime",
+    d: "Code Editor",
+    correct: "c",
   },
   {
-    question: "Dlaczego Matis ma ksywe 'Jurant' ?",
-    a: "Bo ma tak z Bierzmowania",
-    b: "Grał w krzyżakach",
-    c: "po alkoholu patrzy na wszystko z przymrużeniem oka",
-    d: "jest potomkiem Zbycha z Bogdańca",
-    correct: "c"
+    question: "Can we write backend and frontedn in Javascript?",
+    a: "Yes",
+    b: "No",
+    c: "Only in certain situations",
+    d: "We can write only FrontEnd in JavaScript",
+    correct: "a",
   },
   {
-    question: "Ile dni w roku Dejwu jest na L4 ?",
-    a: "Łoo panie",
-    b: "366",
-    c: "pół na pół",
-    d: "na pewno wiecej niż mrówa na urlopie",
-    correct: "d"
+    question: "What does a FrontEnd developer do?",
+    a: "Sells real estate",
+    b: "Sells a network",
+    c: "Developing websites and UI",
+    d: "Create the websites structure, write code, and verify the code works.",
+    correct: "c",
   },
-]
+];
 
-const answerEls = document.querySelectorAll(".answer")
+const answerEls = document.querySelectorAll(".answer");
 const submitBtn = document.querySelector(".submit");
 const questionEl = document.querySelector("h1");
 const popup = document.querySelector(".popup");
 const closePopupBtn = document.querySelector(".close");
-const h2 = document.querySelector("h2")
+const h2 = document.querySelector("h2");
 
 //answers dots (a,b,c,d)
-const answersEl = document.querySelectorAll(".answer")
-
-
-
+const answersEl = document.querySelectorAll(".answer");
 
 // labels
 const a_text = document.getElementById("a_text");
@@ -96,44 +91,39 @@ const d_text = document.getElementById("d_text");
 
 let currentQuiz = 0;
 let score = 0;
-let answer = undefined
+let answer = undefined;
 
 const loadQuiz = () => {
   const currentQuizData = quizData[currentQuiz];
-    questionEl.innerText = currentQuizData.question;
-    a_text.innerText = currentQuizData.a;
-    b_text.innerText = currentQuizData.b;
-    c_text.innerText = currentQuizData.c;
-    d_text.innerText = currentQuizData.d;
-}
+  questionEl.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
+};
 
 loadQuiz();
 
 submitBtn.addEventListener("click", () => {
- 
-answersEl.forEach(answerEl => {
-if(answerEl.checked === false) {
-   return
-} else {
-  answer = answerEl.id;
-if(answer === quizData[currentQuiz].correct) {
-  score++
-}
-  currentQuiz++
-} if(currentQuiz < quizData.length) {
- loadQuiz()
+  answersEl.forEach((answerEl) => {
+    if (answerEl.checked === false) {
+      return;
+    } else {
+      answer = answerEl.id;
+      if (answer === quizData[currentQuiz].correct) {
+        score++;
+      }
+      currentQuiz++;
+    }
+    if (currentQuiz < quizData.length) {
+      loadQuiz();
+    } else {
+      popup.classList.add("appear");
+      h2.textContent = `correct questions: ${score}/${quizData.length}`;
+    }
+  });
+});
 
-} else {
-  popup.classList.add("appear");
-h2.textContent = `poprawne odpowiedzi ${score}/${quizData.length}`
-
-}
-})
-  
-})
-
-
-
-closePopupBtn.addEventListener( "click",() => {
-  location.reload()
-})
+closePopupBtn.addEventListener("click", () => {
+  location.reload();
+});
